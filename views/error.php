8d57cb1c6cf9d199bc2b7a78e2888d9998952b3d
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-12-04 21:56:41 -0500

    ajouter error.php et css

---
+++ views/error.php
@@ -0,0 +1,6 @@
+{{ include ('layouts/header.php', {title:'Registration'})}}
+<div class="conteneur">
+    <h2>Error</h2>
+    <strong class="error">{{ message }}</strong>
+</div>
+{{ include ('layouts/footer.php')}}
