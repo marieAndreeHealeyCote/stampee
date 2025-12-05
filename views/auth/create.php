ebca152190912df8a34575ecebb547b6ee94199e
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-12-04 20:03:01 -0500

    Ajuster CSS validation

--- views/auth/create.php
+++ views/auth/create.php
@@ -1,16 +1,17 @@
 {{ include ('layouts/header.php', {title:'Login'})}}
 <div class="section__login">
-    {% if errors is defined %}
-    <div class="error">
-        <ul>
-            {% for error in errors %}
-            <li>{{ error }}</li>
-            {% endfor %}
-        </ul>
-    </div>
-    {% endif %}
+
     <form method="post" class="section__login__form" novalidate>
         <h2>Login</h2>
+        {% if errors is defined %}
+        <div class="error">
+            <ul>
+                {% for error in errors %}
+                <li>{{ error }}</li>
+                {% endfor %}
+            </ul>
+        </div>
+        {% endif %}
         <label>Username
             <input type="email" name="username" value="{{ user.username }}" placeholder="john@doe.com">
         </label>
