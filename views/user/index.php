4bffe0a6e867f48fa2da60794c9107a6a93a2318
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-12-10 05:58:38 -0500

    création profil , formulaire stamps

--- views/user/index.php
+++ views/user/index.php
@@ -4,15 +4,56 @@
     <aside class="section__aparte">
         <h2>My Profile</h2>
         <section class="section__aparte__certification">
-            <a href="#">
+            <div>
                 <h3>My Stamps</h3>
-            </a>
-            <a href="#">
+                {% for stamp in stamps %}
+                <article>
+                    <div><img src="{{ asset }}img/britishLevant.webp"></div>
+                    <ul>
+                        <li><strong>Name : </strong>{{ stamp.name }}</li>
+                        <li><strong>Year : </strong>{{ stamp.year }}</li>
+                        <li><strong>Certification : </strong>{{ stamp.is_certified }}</li>
+                        <li><strong>Country : </strong>{{ stamp.country }}</li>
+                        <li><strong>Color : </strong>{{ stamp.color }}</li>
+                        <li><strong>Condition : </strong>{{ stamp.condition }}</li>
+                    </ul>
+                    <a href="{{base}}/stamp/edit?id={{stamp.id}}">Edit</a>
+                    <a href="{{base}}/stamp/delete?id={{stamp.id}}">Delete</a>
+                </article>
+                {% else %}
+                <a href="{{base}}/stamp/create">Add a first stamp</a>
+                {% endforeach %}
+            </div>
+            <div>
                 <h3>My Bids</h3>
-            </a>
-            <a href="#">
+                {% for bid in bids %}
+                <article>
+                    TODO
+                </article>
+                {% else %}
+                <a href="{{base}}/auctions">Look for auctions to bid on</a>
+                {% endforeach %}
+            </div>
+            <div>
                 <h3>My auctions</h3>
-            </a>
+                {% for auction in auctions %}
+                <article>
+                    TODO
+                </article>
+                {% else %}
+                <a href="/stamps">Create my first auction</a>
+                {% endforeach %}
+            </div>
+            <div>
+                <h3>My favorites</h3>
+                {% for favorite in favorites %}
+                <article>
+                    TODO
+                </article>
+                {% else %}
+                <a href="{{base}}/auctions">Look for auctions</a>
+                {% endforeach %}
+            </div>
         </section>
     </aside>
 </div>
