1f6fa33505ae32c1f9ce539f9e066715c6fa0a78
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-12-10 20:55:56 -0500

    Corriger texte

--- views/user/index.php
+++ views/user/index.php
@@ -3,19 +3,19 @@
     <h2>Welcome back, {{ session.user_name }}</h2>
     <aside class="section__profil__aparte">
         <a href="{{ base }}/user/my-stamps">
-            <h3>My Stamps</h3>
+            <h3>My stamps</h3>
         </a>
         <a href="{{ base }}/user/my-bids">
-            <h3>My Bids</h3>
+            <h3>My bids</h3>
         </a>
         <a href="{{ base }}/user/my-auctions">
-            <h3>My Auctions</h3>
+            <h3>My auctions</h3>
         </a>
     </aside>
 
     <div class="section__profil">
         <div class="section__profil__timbre">
-            <h3>My Stamps</h3>
+            <h3>My stamps</h3>
             {% for stamp in stamps %}
             <article>
                 <div><img src="{{ asset }}img/britishLevant.webp"></div>
@@ -35,7 +35,7 @@
             {% endfor %}
         </div>
         <div class="section__profil__mise">
-            <h3>My Bids</h3>
+            <h3>My bids</h3>
             {% for bid in bids %}
             <article>
                 TODO
