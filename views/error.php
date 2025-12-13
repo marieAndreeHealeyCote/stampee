c10838337cc21d80a5056841e33931df3a25d45a
Marie-Andrée Healey-Côté <amy146892@hotmail.com>
2025-12-13 12:30:17 -0500

    modification error.php

--- views/error.php
+++ views/error.php
@@ -1,5 +1,7 @@
 {{ include ('layouts/header.php', {title:'Registration'})}}
+
 <div class="conteneur">
+    <div class="error">Error: {{ msg }}</div>
     <img src="{{ asset }}img/404.webp" alt="image erreur 404 ">
 </div>
 {{ include ('layouts/footer.php')}}
